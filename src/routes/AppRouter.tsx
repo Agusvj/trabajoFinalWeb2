c4be0be8afe68020761ed10fe34167d57ed96ea8
AgusVJ <agustinvillanuevajousset@gmail.com>
2025-10-21 11:31:19 -0300

    Navbar y product detail estructura

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import Home from "../pages/Home";
+import ProductDetail from "../pages/ProductDetail";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="/product-detail" element={<ProductDetail />} />
       </Route>
     </Routes>
   );
