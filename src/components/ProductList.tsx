9792d27292712d5623317eb1df1d4216ffacba92
criss <andreapaniagua157@gmail.com>
2025-11-23 02:44:45 -0300

    cambios esteticos footer header productcard

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -26,7 +26,7 @@ export default function ProductList ({category}: ProductListCategory){
     return(
         <div>
             <h2 className="text-2xl font-bold mb-4">{category.title}</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            <div className="mt-4 grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
                 {products.map((product)=>(
                     <ProductCard key= {product.id} product= {product}/>
                 ))}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -86,7 +86,7 @@ export default function Home() {
 
   return (
     <section>
-      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+      <div className=" bg-gray-100 mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
         <header>
           <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
             Nuestros Productos
